a301288e7ac7e2529079e379ab6bb42f946488a4
Bui Long <duclongbuivn@gmail.com>
2018-04-19 15:30:40 +0700

    home

--- app/src/main/java/au/mccann/oztaxreturn/networking/ApiInterface.java
+++ app/src/main/java/au/mccann/oztaxreturn/networking/ApiInterface.java
@@ -2,14 +2,20 @@ package au.mccann.oztaxreturn.networking;
 
 import java.util.List;
 
+import au.mccann.oztaxreturn.model.ImageResponse;
 import au.mccann.oztaxreturn.model.UserReponse;
 import au.mccann.oztaxreturn.rest.response.ApplicationResponse;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
 
 /**
  * Created by CanTran on 5/23/17.
@@ -27,6 +33,17 @@ public interface ApiInterface {
     @POST("application")
     Call<ApplicationResponse> createApplication(@Header("Authorization") String token, @Body RequestBody body);
 
+    @POST("application/duplicate")
+    Call<ApplicationResponse> duplicateApplication(@Header("Authorization") String token, @Body RequestBody body);
+
     @GET("application/list")
     Call<List<ApplicationResponse>> getAllApplication(@Header("Authorization") String token);
+
+    @Multipart
+    @POST("attachment")
+    Call<List<ImageResponse>> uploadImage(@Header("Authorization") String token, @Part List<MultipartBody.Part> files);
+
+    @DELETE("application/{application_id}")
+    Call<Void> deleteApplication(@Header("Authorization") String token, @Path("application_id") int appId);
+
 }
