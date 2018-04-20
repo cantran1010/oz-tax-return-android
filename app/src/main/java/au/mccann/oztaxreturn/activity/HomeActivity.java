3aed718273472e7e3d6874ffac1f021702b8c609
Bui Long <duclongbuivn@gmail.com>
2018-04-20 16:48:04 +0700

    first checkout layout

--- app/src/main/java/au/mccann/oztaxreturn/activity/HomeActivity.java
+++ app/src/main/java/au/mccann/oztaxreturn/activity/HomeActivity.java
@@ -1,6 +1,8 @@
 package au.mccann.oztaxreturn.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,10 +13,13 @@ import au.mccann.oztaxreturn.R;
 import au.mccann.oztaxreturn.fragment.ContactFragment;
 import au.mccann.oztaxreturn.fragment.HomeFragment;
 import au.mccann.oztaxreturn.fragment.NotificationFragment;
+import au.mccann.oztaxreturn.utils.LogUtils;
 import au.mccann.oztaxreturn.utils.TransitionScreen;
+import au.mccann.oztaxreturn.utils.Utils;
 import au.mccann.oztaxreturn.view.TextViewCustom;
 
 public class HomeActivity extends BaseActivity implements View.OnClickListener {
+    private static final String TAG = HomeActivity.class.getSimpleName();
     private DrawerLayout drawer;
     private ImageView imgHome, imgContact, imgNotification, imgBack, imgNavigation;
     private TextViewCustom tvHome, tvContact, tvNotification, tvTitle;
@@ -58,13 +63,68 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
 
     }
 
+    boolean doubleBackToExitPressedOnce = false;
+
     @Override
     public void onBackPressed() {
         if (drawer.isDrawerOpen(GravityCompat.END)) {
             drawer.closeDrawer(GravityCompat.END);
-            super.onBackPressed();
+//            super.onBackPressed();
         } else {
-            super.onBackPressed();
+//            super.onBackPressed();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (fragmentManager.getBackStackEntryCount() == 0) {
+//            super.onBackPressed();
+//            overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+
+            if (doubleBackToExitPressedOnce) {
+                this.finish();
+                return;
+            }
+
+            this.doubleBackToExitPressedOnce = true;
+
+            Utils.showLongToast(this, getString(R.string.back_to_exit), false, false);
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        } else {
+
+            String fragmentTag = fragmentManager.getBackStackEntryAt(fragmentManager.getBackStackEntryCount() - 1).getName();
+            LogUtils.d(TAG, "fragmentTag : " + fragmentTag);
+            if (fragmentTag.equals("au.mccann.oztaxreturn.fragment.HomeFragment")
+                    || fragmentTag.equals("au.mccann.oztaxreturn.fragment.ContactFragment")
+                    || fragmentTag.equals("au.mccann.oztaxreturn.fragment.NotificationFragment")) {
+
+                if (doubleBackToExitPressedOnce) {
+                    this.finish();
+                    return;
+                }
+
+                this.doubleBackToExitPressedOnce = true;
+
+                Utils.showLongToast(this, getString(R.string.back_to_exit), false, false);
+
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        doubleBackToExitPressedOnce = false;
+                    }
+                }, 2000);
+
+            } else {
+                fragmentManager.popBackStack();
+            }
+
         }
 
     }
@@ -124,17 +184,17 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener {
 
             case R.id.layout_home:
                 updateMenu(1);
-                openFragment(R.id.layout_container, HomeFragment.class, false, new Bundle(), TransitionScreen.FADE_IN);
+                openFragment(R.id.layout_container, HomeFragment.class, true, new Bundle(), TransitionScreen.FADE_IN);
                 break;
 
             case R.id.layout_contact:
                 updateMenu(2);
-                openFragment(R.id.layout_container, ContactFragment.class, false, new Bundle(), TransitionScreen.FADE_IN);
+                openFragment(R.id.layout_container, ContactFragment.class, true, new Bundle(), TransitionScreen.FADE_IN);
                 break;
 
             case R.id.layout_notification:
                 updateMenu(3);
-                openFragment(R.id.layout_container, NotificationFragment.class, false, new Bundle(), TransitionScreen.FADE_IN);
+                openFragment(R.id.layout_container, NotificationFragment.class, true, new Bundle(), TransitionScreen.FADE_IN);
                 break;
 
             case R.id.img_back:
