9482a1ffa74939b8484c7a3014d046372ddf717d
Bui Long <duclongbuivn@gmail.com>
2018-05-17 15:55:15 +0700

    fix bug 74 on Jira

--- app/src/main/java/au/mccann/oztaxreturn/fragment/review/family/ReviewFamilyHealthPrivateFragment.java
+++ app/src/main/java/au/mccann/oztaxreturn/fragment/review/family/ReviewFamilyHealthPrivateFragment.java
@@ -291,6 +291,7 @@ public class ReviewFamilyHealthPrivateFragment extends BaseFragment implements V
                     });
                 } else {
                     doNext();
+                    return;
                 }
             }
         }
