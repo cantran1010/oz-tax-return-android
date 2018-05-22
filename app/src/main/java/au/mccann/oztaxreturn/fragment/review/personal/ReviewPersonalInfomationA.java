149e0a9fa66f5ea45cb119bc093d0d67508f4b39
Bui Long <duclongbuivn@gmail.com>
2018-05-22 14:39:31 +0700

    conflict

--- app/src/main/java/au/mccann/oztaxreturn/fragment/review/personal/ReviewPersonalInfomationA.java
+++ app/src/main/java/au/mccann/oztaxreturn/fragment/review/personal/ReviewPersonalInfomationA.java
@@ -12,7 +12,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
